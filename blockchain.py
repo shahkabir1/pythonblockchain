131bc0f9396c91b01a74b0f5feb8723a064e4058
Shah <shahkabir2003@gmail.com>
2025-01-02 15:50:46 -0500

    Added PoW

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
@@ -39,4 +41,17 @@ def last_block(self):
 
 @staticmethod
 def hash(block):
-    
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
+
+def proof_of_work(self, last_proof):
+    proof = 0
+    while self.valid_proof(last_proof, proof) is False:
+        proof += 1
+
+    return proof
+
+def valid_proof(self, last_proof, proof):
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
