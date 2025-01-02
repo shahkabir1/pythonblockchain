cbb2dfe726522ff3d2f7af2232b2349d578253f3
Shah <shahkabir2003@gmail.com>
2025-01-02 15:49:08 -0500

    Added implementations of methods

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,30 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-def new_block(self, sender, recipient, amount):
+        self.new_block(previous_hash=1, proof=100)
+
+def new_block(self, proof, previous_hash=None):
+    block = {
+        'index': len(self.chain) + 1,
+        'timestamp': time(),
+        'transactions': self.current_transactions,
+        'proof': proof,
+        'previous_hash': self.hash(self.chain[-1]),
+    }
+
+    self.current_transactions = []
+
+    self.chain.append(block)
+
+    return block
+
+def new_transaction(self):
     self.current_transactions.append({
         'sender': sender,
         'recipient': recipient,
@@ -12,13 +33,10 @@ def new_block(self, sender, recipient, amount):
 
     return self.last_block['index'] + 1
 
-def new_transaction(self):
-    pass
+@property
+def last_block(self):
+    return self.chain[-1]
 
 @staticmethod
 def hash(block):
-    pass
-
-@property
-def last_block(self):
-    pass
+    
