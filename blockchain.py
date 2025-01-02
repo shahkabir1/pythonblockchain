584b7b9bf62d020baba394a9ae3a02c3a47088da
Shah <shahkabir2003@gmail.com>
2025-01-02 15:43:02 -0500

    first commit

---
+++ blockchain.py
@@ -0,0 +1,24 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+def new_block(self, sender, recipient, amount):
+    self.current_transactions.append({
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
+    })
+
+    return self.last_block['index'] + 1
+
+def new_transaction(self):
+    pass
+
+@staticmethod
+def hash(block):
+    pass
+
+@property
+def last_block(self):
+    pass
