02839a0ee81ea0090516f1a841bf39202b91b940
Shah <shahkabir2003@gmail.com>
2025-01-02 15:54:06 -0500

    Implemented server functionality

--- blockchain.py
+++ blockchain.py
@@ -55,3 +55,29 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We shall mine a block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We shall add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
