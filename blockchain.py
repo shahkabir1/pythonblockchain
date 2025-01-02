4100bfe66b244395aeabc346c5c234e5c205d935
Shah <shahkabir2003@gmail.com>
2025-01-02 15:51:34 -0500

    Incorrect indentation!! Shah you forgot how to make Classes!!!

--- blockchain.py
+++ blockchain.py
@@ -11,47 +11,47 @@ class Blockchain(object):
 
         self.new_block(previous_hash=1, proof=100)
 
-def new_block(self, proof, previous_hash=None):
-    block = {
-        'index': len(self.chain) + 1,
-        'timestamp': time(),
-        'transactions': self.current_transactions,
-        'proof': proof,
-        'previous_hash': self.hash(self.chain[-1]),
-    }
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': self.hash(self.chain[-1]),
+        }
 
-    self.current_transactions = []
+        self.current_transactions = []
 
-    self.chain.append(block)
+        self.chain.append(block)
 
-    return block
+        return block
 
-def new_transaction(self):
-    self.current_transactions.append({
-        'sender': sender,
-        'recipient': recipient,
-        'amount': amount
-    })
+    def new_transaction(self):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
 
-    return self.last_block['index'] + 1
+        return self.last_block['index'] + 1
 
-@property
-def last_block(self):
-    return self.chain[-1]
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
-@staticmethod
-def hash(block):
-    block_string = json.dumps(block, sort_keys=True).encode()
-    return hashlib.sha256(block_string).hexdigest()
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
-def proof_of_work(self, last_proof):
-    proof = 0
-    while self.valid_proof(last_proof, proof) is False:
-        proof += 1
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
 
-    return proof
+        return proof
 
-def valid_proof(self, last_proof, proof):
-    guess = f'{last_proof}{proof}'.encode()
-    guess_hash = hashlib.sha256(guess).hexdigest()
-    return guess_hash[:4] == "0000"
+    def valid_proof(self, last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
